7bc4cd9cb77e7789c1c0f7be3870d981916e6782
Patrick Orwin <patrick.orwin@revature.net>
2022-01-19 18:39:37 -0500

    Created our tables

--- src/main/java/com/revature/models/PokeDex.java
+++ src/main/java/com/revature/models/PokeDex.java
@@ -1,5 +1,17 @@
 package com.revature.models;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "pokedex")
 public class PokeDex {
-
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int pokedex_id;
+	private int pokemon_id;
+	private int user_id;
 }
--- src/main/java/com/revature/models/User.java
+++ src/main/java/com/revature/models/User.java
@@ -1,5 +1,30 @@
 package com.revature.models;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users")
 public class User {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int user_id;
+	@Column(unique=true)
+	private String username;
+	private String password;
+	private String first_name;
+	private String last_name;
+	@Column(unique=true)
+	private String email_address;
+	private String phone_number;
+	private String physical_address;
+	private String credit_card_number;
+	private String credit_card_name;
+	
+	
 }

4e4898a7d0018582c14856e651495cfeed09575f
Patrick Orwin <patrick.orwin@revature.net>
2022-01-20 17:42:46 -0500

    Built out Auth Controller and Services

--- src/main/java/com/revature/services/AuthServices.java
+++ src/main/java/com/revature/services/AuthServices.java
@@ -1,5 +1,31 @@
 package com.revature.services;
 
-public class AuthServices {
+import java.util.List;
 
+import com.revature.models.User;
+import com.revature.repositories.UserDAO;
+
+public class AuthServices {
+	UserDAO ud = new UserDAO();
+	
+	public int login(String username, String password) {   
+    	List<User> CheckUsername = ud.getUserByUsername(username);
+    	try{
+    		User Check = CheckUsername.get(0);
+    		String Username = Check.getUsername();
+        	String Password = Check.getPassword();
+            if (Username.equals(username) && Password.equals(password)) {
+            	return 1;
+            }
+            else {
+            	return 0;
+            }
+    	}
+    	catch(IndexOutOfBoundsException e) {
+    		System.out.println("That Username or Password didn't match our system");
+    		return 0;
+    	}
+    	
+    }
+	
 }

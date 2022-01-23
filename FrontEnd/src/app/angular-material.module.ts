1d906dbcc32057bb91deeb99dede3a236dc05115
david yuen <davidyuen0305@gmail.com>
2022-01-22 21:08:29 -0800

    messed around with angular, created sample navbar and searchbar for front page, also template for user info, no functionality yet.

---
+++ FrontEnd/src/app/angular-material.module.ts
@@ -0,0 +1,33 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+// import { MatCommonModule, MatNativeDateModule } from '@angular/material/core'; //don't need
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+
+const materialModules = [
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatTooltipModule,
+    MatButtonModule,
+    MatSnackBarModule,
+    MatToolbarModule
+];
+
+@NgModule({
+    imports: [
+        CommonModule,
+        ...materialModules
+    ],
+    exports: [
+        ...materialModules
+    ]
+})
+
+export class AngularMaterialModule { }
--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser'; //BrowserModule is required for all browser based application
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
@@ -11,7 +11,12 @@ import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { SearchbarComponent } from './components/searchbar/searchbar.component';
 import { UserprofileComponent } from './components/userprofile/userprofile.component';
-import { HeaderComponent } from './components/header/header.component';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// AngularMaterialModule 
+import { AngularMaterialModule } from './angular-material.module';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   //declarations : the components, directives, and pipes that belong to this NgModule
@@ -24,7 +29,7 @@ import { HeaderComponent } from './components/header/header.component';
     RegisterComponent,
     SearchbarComponent,
     UserprofileComponent,
-    HeaderComponent,
+    NavbarComponent,
   ],
   //imports : other modules whose exported classes are needed by component templates declared in this NgModule
   imports: [
@@ -32,6 +37,8 @@ import { HeaderComponent } from './components/header/header.component';
     AppRoutingModule, // for routing
     HttpClientModule, // for http GET,POST,PUT,DELETE
     FormsModule, // for forms
+    AngularMaterialModule,
+    BrowserAnimationsModule
   ],
   //app.module.ts doesn't require exports here b/c it's the root of the application but it is a subset of declarations
   // that should be visible and usable in the component templates of other NgModules
@@ -41,6 +48,8 @@ import { HeaderComponent } from './components/header/header.component';
   providers: [],
   //bootstrap: the main application view, called the root component which hosts all other app views. Only the root NgModule should set the
   //boostrap property
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  // schemas: [CUSTOM_ELEMENTS_SCHEMA]
+  schemas: [NO_ERRORS_SCHEMA]
 })
 export class AppModule { }

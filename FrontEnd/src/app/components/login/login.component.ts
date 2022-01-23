3f4d8615ecebb14acb645374be565918098f9c1d
Agustin Guzman <agustin.guzman@outlook.com>
2022-01-23 18:02:09 -0500

    agustin01

--- FrontEnd/src/app/components/login/login.component.ts
+++ FrontEnd/src/app/components/login/login.component.ts
@@ -6,8 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  //uname:string;
-  //upass:string;
+  uname:string;
+  upass:string;
   constructor() { }
 
   ngOnInit(): void {

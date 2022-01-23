09180f726c92ba49bf73f2068bf8454a556d367e
Agustin Guzman <agustin.guzman@outlook.com>
2022-01-23 17:31:31 -0500

    Agustin01 I tryed to create a test of login page

--- FrontEnd/src/app/components/login/login.component.ts
+++ FrontEnd/src/app/components/login/login.component.ts
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
+  //uname:string;
+  //upass:string;
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  /*
+  Loginusr(){
+    if (this.uname =="GRAGRA" && this.upass =="GRAGRA")
+    {
+      console.log("You can Pass")
+    }
+    else
+    {
+      console.log("You are Unwelcome")
+    }
+  }*/
 }

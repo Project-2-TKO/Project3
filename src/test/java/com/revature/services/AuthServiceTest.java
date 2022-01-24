b2915077981f44045e2b851b432af24fa4cf2aa5
Payton Dull <payton.dull@revature.net>
2022-01-24 17:29:55 -0500

    Outline for JUnit

--- src/test/java/com/revature/services/AuthServiceTest.java
+++ src/test/java/com/revature/services/AuthServiceTest.java
@@ -1,4 +1,15 @@
 package com.revature.services;
+import static org.junit.Assert.*;
+
+import static org.mockito.Mockito.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
 
 public class AuthServiceTest {
 
--- src/test/java/com/revature/util/HibernateSessionTest.java
+++ src/test/java/com/revature/util/HibernateSessionTest.java
@@ -1,5 +1,7 @@
 package com.revature.util;
 
+
+
 public class HibernateSessionTest {
 
 }

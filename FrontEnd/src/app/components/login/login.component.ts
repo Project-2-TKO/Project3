a11c81b228e426437c3dadf6a3d9a3451e5ba94c
Agustin Guzman <agustin.guzman@outlook.com>
2022-01-31 17:56:21 -0500

    Agustin01 login completed

--- FrontEnd/src/app/components/login/login.component.ts
+++ FrontEnd/src/app/components/login/login.component.ts
@@ -1,4 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { catchError} from 'rxjs';
+import { Observable } from 'rxjs';
+import { HttpHeaders } from '@angular/common/http';
+import { of } from 'rxjs';
+import { error } from '@angular/compiler/src/util';
+import { Router } from '@angular/router';
+const httpOptions   = {
+  headers: new HttpHeaders({
+    'Content-Type':  'application/json',
+    'Access-Control-Allow-Headers': 'Content-type:application/json',
+    'Access-Control-Allow-Methods': 'GET',
+    'Access-Control-Allow-Origin': '*'
+  })
+};
+
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -8,25 +25,47 @@ import { Component, OnInit } from '@angular/core';
 export class LoginComponent implements OnInit {
 
   username!: String;
-  pssword!:String
-
-
-  constructor( ) {
+  password!:String;
+  result!: boolean;
+  user = {username: String, password: String};
+  Credentials = {withCredentials: true};
+  response : any ;
+  constructor(private _http : HttpClient, private router : Router ) {
 
    }
 
-  ngOnInit(): void {
-
+  ngOnInit(): void{
+   // this.response=this._http.post("localhost:3000/login",this.user, this.Credentials );
   }
 
-  Loginusr(): void {
-    if (this.username =="GRAGRA" && this.pssword =="GRAGRA")
+  Loginusr(){
+    let user = {username: this.username,
+                password: this.password};
+
+    let Credentials = {withCredentials: true};
+    console.log(this.username);
+    console.log(this.password);
+    console.log(user);
+    console.log(Credentials);
+    let response = this._http.post<any>("http://localhost:3000/login", user, httpOptions ).subscribe ({
+      next: (v) => this.router.navigate(['/frontpage']),  //console.log("reponse rcieved"),
+      error: (e) => console.error(e),
+      complete: () => console.info('Complete')
+    }
+    );
+    console.log(response);
+
+     //let response = this._http.post<any>("localhost:3000/login", user, Credentials );
+     //.pipe(Map(this.user), catchError(this.handleErrorObservable));
+      console.log(response);
+   /* if (this.username =="GRAGRA" && this.password =="GRAGRA")
     {
       console.log("You can Pass")
+
     }
     else
     {
       console.log("You are Unwelcome")
-    }
+    }*/
   }
 }

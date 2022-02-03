2de932f75695254a58633270d2a7526b73a6ad32
meenakshipate <p_meena@aol.com>
2022-02-03 13:22:41 -0600

    resetpassword

--- FrontEnd/src/app/components/resetpassword/resetpassword.component.ts
+++ FrontEnd/src/app/components/resetpassword/resetpassword.component.ts
@@ -7,9 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResetpasswordComponent implements OnInit {
 
+  username: String;
+  password:String;
+  user = {username: String, password: String};
   constructor() { }
 
   ngOnInit(): void {
+  }
+  resetpasswordnusr(){
+    let user = {username: this.username,
+                password: this.password};
+
+    let Credentials = {withCredentials: true};
+    console.log(this.username);
+    console.log(this.password);
+    console.log(user);
+    console.log(Credentials);
+    let response = this._http.put<any>("http://localhost:3000/login", user, httpOptions ).subscribe ({
+      next: (v) => this.router.navigate(['/frontpage']),  //console.log("reponse rcieved"),
+      error: (e) => console.error(this.msgError="Invalid Credentials, Please Enter a Valid User Name And/or Password"),
+      complete: () => console.info('Complete')
+    }
+    );
   }
 
 }

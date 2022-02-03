2ec2d816d8b81e97f050e1db73677fce4ecd6c38
dyuen510 <davidyuen0305@gmail.com>
2022-02-03 07:18:17 -0800

    front page output and material cards. planning to change to bootstrap cards still testing.

--- FrontEnd/src/app/components/pokemon-container/pokemon-container.component.ts
+++ FrontEnd/src/app/components/pokemon-container/pokemon-container.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { PokeDataService } from 'src/app/poke-data.service';
 import { Pokemon } from '../../pokemon';
-import { map } from 'rxjs';
+import { map, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-pokemon-container',
@@ -10,15 +10,48 @@ import { map } from 'rxjs';
   styleUrls: ['./pokemon-container.component.css']
 })
 export class PokemonContainerComponent implements OnInit {
-  
+  collapsed = true;
   public name: string = '';
   public pokemon: any = "placeholder";
+  public pokemonArray: Array<Pokemon> = [];
+  public pokeDataArray: any[] = []; //might be this
+
+  p: number = 0;
+  totalPokemon: number;
 
   constructor(private ps: PokeDataService) {}
   
   ngOnInit(): void{
-    
-  }
+    // this.ps.getAllPokemons()
+    // .subscribe((response: any) => {
+    //   response.results.forEach(result => {
+    //     this.ps
+    //   })
+    // })
+    this.getAllPokemons();
+}
+
+getAllPokemons(){
+  console.log(this.p);
+  this.ps.getAllPokemons(151, this.p + 0).subscribe(
+    (response: any) => {
+      this.totalPokemon = response.count;
+      
+      // console.log(response);
+      response.results.forEach((result: { name: string; }) => {
+        this.ps.getDetails(result.name)
+        .subscribe((data: any) => {
+          this.pokeDataArray.push(data);
+          console.log(this.pokeDataArray);
+      })
+    }
+  
+  )
+
+})
+}
+
+//getting pokemon by name in search bar
 getPokemon():void{
   this.ps.getPokemonFromApi(this.name).subscribe(
 
@@ -36,8 +69,34 @@ getPokemon():void{
       this.pokemon = null
       console.log("It got away!!!")
     }
-
-
   )
 }
+showId(num: number){
+  console.log(num);
+}
+getRandomNum(){
+  return (Math.random()*(500)+1.00).toFixed(2);
+}
+//get all pokemons into an array
+// getAllPokemons(): void {
+//   this.ps.getAllPokemons().subscribe(
+//     (data:any) => {
+//       for( let details of data ){
+//         this.pokemonArray.push(details);
+//         // console.log(this.pokemonArray);
+//       }
+//       for(let pokeData of this.pokemonArray){
+//         this.pokeDataArray.push(pokeData);
+//         console.log(pokeData.id);
+//         console.log(pokeData.name);
+//         console.log(pokeData.types);
+        
+//       }
+//       console.log(this.pokeDataArray)
+//       // console.log(this.pokemonArray); // now we can use *ngFor to loop through this array?
+//     }
+//   )
+// }
+
+
 }
--- FrontEnd/src/app/components/searchbar/searchbar.component.ts
+++ FrontEnd/src/app/components/searchbar/searchbar.component.ts
@@ -1,17 +1,70 @@
 import { Component, OnInit } from '@angular/core';
+import { PokeDataService } from 'src/app/poke-data.service';
 
 @Component({
   selector: 'app-searchbar',
-  templateUrl: './searchbar.component.html',
+  template:`
+  <div>
+        <div class="row" style="margin-left:50%">
+        <!-- Two way binding this input field to a variable called input in pokedex.component.ts -->
+        <input class="col-lg-3" type="string" [(ngModel)] = "name"/>
+        <!--Event binding on this button to call the getPoke() function in pokedex.component.ts-->
+        <button class=" btn btn-dark col-lg-2" (click)= "getPokemon()">Choose Pokemon</button>
+    </div>
+
+    
+        <div class='card_body' *ngIf="pokemon">
+            <mat-card class='pokemon_cards'>
+                <mat-card-title>
+                    {{pokemon.name}}
+                </mat-card-title>
+                <mat-card-content>
+                    {{pokemon.type}}
+                </mat-card-content>
+                <img mat-card-sm-image src="{{pokemon.sprites.front_default}}">
+                <mat-card-subtitle>
+                    $180.99
+                </mat-card-subtitle>
+                <mat-card-actions>
+                    <button mat-raised-button>Add to Cart</button>
+                </mat-card-actions>
+            </mat-card>
+        </div>
+
+</div>
+
+  `,
   styleUrls: ['./searchbar.component.css']
 })
 export class SearchbarComponent implements OnInit {
 
-  public searchInput: string = '';
+  public name: string = '';
+  public pokemon: any = "placeholder";
   
-  constructor() { }
+  constructor(private ps: PokeDataService) { }
 
   ngOnInit(): void {
   }
 
+
+  //getting pokemon by name in search bar
+getPokemon():void{
+  this.ps.getPokemonFromApi(this.name).subscribe(
+
+    //get the data out of the observable that we subscribe to, and put it into a Pokemon object
+    (data:any) => {
+      let response:String = data.status;//gets the status code
+      console.log(response);
+      //assign it to our pokemon variable above
+      this.pokemon = data.body;
+      //we may have to do something with sprites
+      console.log(this.pokemon) //will be helpful for debugs
+    },
+
+    () => { //incase of errors, set pokemon object to null since we didn't get anything back
+      this.pokemon = null
+      console.log("It got away!!!")
+    }
+  )
+}
 }

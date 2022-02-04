5250d21e3c24f0bbc3003dd0d262fa6403cbf6d7
meenakshipate <p_meena@aol.com>
2022-02-04 16:53:44 -0600

    resetpassword not working

--- FrontEnd/src/app/components/resetpassword/resetpassword.component.ts
+++ FrontEnd/src/app/components/resetpassword/resetpassword.component.ts
@@ -1,11 +1,11 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { catchError} from 'rxjs';
 import { of } from 'rxjs';
 import { error } from '@angular/compiler/src/util';
-
+import { User } from 'src/app/models/user';
 
 const httpOptions   = {
   headers: new HttpHeaders({
@@ -24,65 +24,89 @@ const httpOptions   = {
 export class ResetpasswordComponent implements OnInit {
   
   username!: String;
-  password?: String;
-  email_address!:String;
-  credit_card_name!:String;
-  credit_card_number!:String;
-  first_name!:String;
-  Last_name!:String;
-  Phone_number!:String;
-  Physical_address!: String;
-  user = {username: String,
+  password!: String;
+  email!:String;
+  card_name!:String;
+  card_number!:String;
+  firstname!:String;
+  Lastname!:String;
+  Phonenumber!:String;
+  Physicaladdress!: String;
+  usrID!: String;
+  user = {user_id:String,
+          username: String,
           password: String,
           email_address: String,
           credit_card_name :String,
           credit_card_number : String,
           first_name: String,
-          Last_name: String,
-          Phone_number: String,
-          Physical_address: String
+          last_name: String,
+          phone_number: String,
+          physical_address: String
         };
   response : any ;
   msgError ="";
   Credentials = {withCredentials: true};
   constructor(private _http : HttpClient, private router : Router) { }
 
+  //s_username: any;
+ 
+  getUserByUsername(username : any):Observable<HttpResponse<User>>{
+    return this._http.get("http://localhost:3000/user/username/" +username+"/", {observe: "response"}) as Observable<HttpResponse<User>>
+    }
+
   ngOnInit(): void {
-
   }
-
-
-  resetpasswordusr(){
-   // if(this.username == this.username)
-    //{
-      let userID=1;
-      let user = {username:      this.username,
+    resetpasswordusr(){
+    console.log(this.username);
+    console.log("this the user name");
+     this.getUserByUsername(this.username).subscribe(
+      (data:any) =>{
+        this.user = data;
+        //let response: String = data.response;
+        console.log(this.user)
+        this.user = data.body;
+        for(let usr of data.body)
+        {
+            this.username= usr.username;
+            this.email=usr.email_address;
+            this.firstname=usr.first_name;
+            this.Lastname=usr.Last_name;
+            this.Phonenumber=usr.Phone_number;
+            this.Physicaladdress=usr.Physical_address;
+            this.card_number=usr.credit_card_number;
+            this.card_name=usr.credit_card_name;
+            this.usrID=usr.user_id;
+          console.log(this.usrID);
+        }
+      }
+    );
+    
+    console.log(this.usrID);
+      let user = {user_id: this.usrID,
+                  username:      this.username,
                   password:      this.password,
-                  email_address: this.email_address,
-                  credit_card_name : this.credit_card_name,
-                  credit_card_number : this.credit_card_number,
-                  first_name: this.first_name,
-                  Last_name: this.Last_name,
-                  Phone_number: this.Phone_number,
-                  Physical_address: this.Physical_address
+                  email_address: this.email,
+                  credit_card_name : this.card_name,
+                  credit_card_number : this.card_number,
+                  first_name: this.firstname,
+                  Last_name: this.Lastname,
+                  Phone_number: this.Phonenumber,
+                  Physical_address: this.Physicaladdress
                 };
       console.log(this.username);
       console.log(this.password);
       console.log(user);
       let Credentials = {withCredentials: true};
-      let response =this._http.put<any>("http://localhost:3000/user/userID",user,httpOptions,).subscribe (
+      let response =this._http.put<any>("http://localhost:3000/reset" ,user ,httpOptions,).subscribe (
         {
-          next: (v) => this.router.navigate(['/']),  //console.log("reponse rcieved"),
+          next: (v) => this.router.navigate(['/']),  
           error: (e) => console.error(this.msgError="User name or password is undefined "),
           complete: () => console.info('Complete')
         });
 
       console.log(response);
- /* }
-  else
-  {
-    this.msgError="passwords does not update, please revalidate your password"
-  }*/
+ 
   }
-
+  
 }

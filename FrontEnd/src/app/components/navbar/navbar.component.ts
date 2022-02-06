ab3f51c335922e7c89bdee16bec3354eb396aa22
dyuen510 <davidyuen0305@gmail.com>
2022-02-05 22:29:53 -0800

    adjusted css on login, register, pokedex and added string interpolaton on navbar user selection as username.

--- FrontEnd/src/app/components/navbar/navbar.component.ts
+++ FrontEnd/src/app/components/navbar/navbar.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
+  user = window.localStorage.getItem("username") ;
   constructor() { }
 
   ngOnInit(): void {

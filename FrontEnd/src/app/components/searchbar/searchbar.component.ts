b539a0219ccd3617357d7739d6189ac12a34f156
dyuen510 <davidyuen0305@gmail.com>
2022-02-17 09:46:02 -0800

    completed searchbar wishlist.

--- FrontEnd/src/app/components/searchbar/searchbar.component.ts
+++ FrontEnd/src/app/components/searchbar/searchbar.component.ts
@@ -1,6 +1,19 @@
+import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from 'src/app/models/user';
 import { PokeDataService } from 'src/app/poke-data.service';
 import { Pokemon } from 'src/app/pokemon';
+
+
+const httpOptions   = {
+  headers: new HttpHeaders({
+    'Content-Type':  'application/json',
+    'Access-Control-Allow-Headers': 'Content-type:application/json',
+    'Access-Control-Allow-Methods': 'GET',
+    'Access-Control-Allow-Origin': '*'
+  })
+};
 
 @Component({
   selector: 'app-searchbar',
@@ -8,17 +21,52 @@ import { Pokemon } from 'src/app/pokemon';
   styleUrls: ['./searchbar.component.css']
 })
 export class SearchbarComponent implements OnInit {
+  
+  //for wishlist
+  userId = String;
+  public username = window.localStorage.getItem("username");
+  pokemon_id: any = 0;
+  //user object
+  user = {user_id: String,
+    username: String,
+    password: String,
+    email_address: String,
+    credit_card_name :String,
+    credit_card_number : String,
+    first_name: String,
+    last_name: String,
+    phone_number: String,
+    physical_address: String
+};
+  Credentials = {withCredentials: true};
+
 
   isVisible: boolean = false;
   public name: string = '';
   public pokemon: any = "placeholder";
   
-  constructor(private ps: PokeDataService) { }
+  constructor(private ps: PokeDataService, private _http : HttpClient) { }
 
   ngOnInit(): void {
+    console.log(this.username);
+    let curr_user = this.user;
+
+    this.getUserByUsername(this.username).subscribe(
+      (data:any) => {
+        this.user = data;
+        console.log(this.user);
+        for(let response of data.body){
+          this.userId = response.user_id;
+        }
+        console.log(this.userId)
+      }
+    )
   }
 
-
+  getUserByUsername(username : any):Observable<HttpResponse<User>>{
+    return this._http.get("http://localhost:3000/user/username/" +username+"/", {observe: "response"}) as Observable<HttpResponse<User>>
+    }
+    
   //getting pokemon by name in search bar
 getPokemon():void{
   this.isVisible = true;
@@ -32,7 +80,7 @@ getPokemon():void{
       //assign it to our pokemon variable above
       this.pokemon = data.body;
       //we may have to do something with sprites
-      console.log(this.pokemon) //will be helpful for debugs
+      console.log(this.pokemon.id) //will be helpful for debugs
     },
 
     () => { //incase of errors, set pokemon object to null since we didn't get anything back
@@ -50,4 +98,27 @@ addPokemon(pokemon: Pokemon){
   console.log(this.ps.totalCost)
   console.log(typeof this.ps.totalCost)
 }
+
+addToWishList(pokemon: Pokemon){
+  console.log(pokemon.id);
+  let user = {
+    user_id: this.userId,
+    username: this.username
+  }
+  let wishlist = {
+    pokemon_id: pokemon.id,
+    user: user
+  }
+  console.log(user);
+  let Credentials = {withCredentials: true};
+  let response = this._http.post<any>("http://localhost:3000/wishlist", wishlist, httpOptions).subscribe(
+    {
+      next: (v) => console.log("Done"),
+      error: (e) => console.log("oops"),
+      complete: () => console.log("Complete")
+    }
+  );
+  console.log(response);
+}
+
 }
--- FrontEnd/src/app/components/wishlist/wishlist.component.ts
+++ FrontEnd/src/app/components/wishlist/wishlist.component.ts
@@ -1,5 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http';
+import { Pokedex, UserId, Wishlist } from 'src/app/models/pokedex';
+import { Observable } from 'rxjs';
 import { PokeDataService } from 'src/app/poke-data.service';
+
+
+const httpOptions   = {
+  headers: new HttpHeaders({
+    'Content-Type':  'application/json',
+    'Access-Control-Allow-Headers': 'Content-type:application/json',
+    'Access-Control-Allow-Methods': 'GET',
+    'Access-Control-Allow-Origin': '*'
+  })
+};
 
 @Component({
   selector: 'app-wishlist',
@@ -8,18 +21,58 @@ import { PokeDataService } from 'src/app/poke-data.service';
 })
 export class WishlistComponent implements OnInit {
 
-  wishList: any = [];
+  wishlist: any = [];
+  user = window.localStorage.getItem("username") ;
+  userId: string;
+  pokemonId: string;
+  userInfo: any = [];
+  pokemon_id: any = [];
+  public pid: string = '';
+  // pokedexInventory: any = [];
 
-  pokemon: any = null;
+  pokemonArray: any = [];
+  pokemonArray2: any = [];
+
+  response : any ;
+  msgError ="";
+  Credentials = {withCredentials: true};
   
-  constructor(private ps: PokeDataService) {}
+  constructor(private _http : HttpClient, private ps: PokeDataService) {}
 
-  ngOnInit(): void{
+  ngOnInit(): void {
+    this.getUserId(this.user).subscribe(
+      (data:any) => {
+        this.userInfo = data;
+        // console.log(this.userInfo);
+        for(let response of data.body){
+          this.userId = response.user_id;
+          this.getWishlistByUserId(this.userId).subscribe(
+            (data2:any) => {
+              this.wishlist = data2;
+              console.log(this.wishlist);
+              data2.body.forEach((result: {pokemon_id: string}) => {
+                // console.log(result.pokemon_id);
+                this.ps.getPokemonById(result.pokemon_id).subscribe(
+                  (res:any) => {
+                    
+                    this.pokemonArray.push(res.body);
+                    console.log(this.pokemonArray);
+                  }
 
-    this.pokemon = this.ps.pokemon
-
-    this.wishList = this.ps.wishList
-
+                )
+              })
+            }
+          )
+        }
+      }
+    )
   }
 
+  getWishlistByUserId(userId: any):Observable<HttpResponse<Pokedex>>{
+    return this._http.get("http://localhost:3000/wishlist/user/" + userId, {observe: "response"}) as Observable<HttpResponse<Wishlist>>
+  }
+
+  getUserId(user:any):Observable<HttpResponse<UserId>>{
+    return this._http.get("http://localhost:3000/user/username/" + user, {observe: "response"}) as Observable<HttpResponse<UserId>>
+  }
 }

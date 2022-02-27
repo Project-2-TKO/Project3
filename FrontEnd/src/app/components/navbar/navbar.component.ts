03e9f565cd168c9bbf980b92bdd623468297de0b
Agustin Guzman <agustin.guzman@outlook.com>
2022-02-27 18:57:58 -0500

    afternoon try to get

--- FrontEnd/src/app/components/navbar/navbar.component.ts
+++ FrontEnd/src/app/components/navbar/navbar.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
@@ -8,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 export class NavbarComponent implements OnInit {
 
   user = window.localStorage.getItem("username") ;
-  constructor() { }
+  constructor(private _afAuth: AngularFireAuth) { }
 
   ngOnInit(): void {
-  }
 
+  }
+  logout(): void {
+    this._afAuth.signOut();
+  }
 }

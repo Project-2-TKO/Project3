7491bd87bd985f1d65752f82defc099e60fc9f9b
Agustin Guzman <agustin.guzman@outlook.com>
2022-02-27 00:23:02 -0500

    login in firebase

--- FrontEnd/src/app/components/login/login.component.ts
+++ FrontEnd/src/app/components/login/login.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { catchError} from 'rxjs';
+import { catchError, delay} from 'rxjs';
 import { Observable } from 'rxjs';
 import { HttpHeaders } from '@angular/common/http';
 import { of } from 'rxjs';
@@ -12,6 +12,8 @@ import { AuthserviceService } from 'src/app/services/authservice.service';
 import { AngularFireAuthModule } from "@angular/fire/compat/auth";
 import firebase from 'firebase/compat/app';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { waitForAsync } from '@angular/core/testing';
+import { stringify } from 'querystring';
 
 const httpOptions   = {
   headers: new HttpHeaders({
@@ -41,7 +43,7 @@ export class LoginComponent implements OnInit {
   response : any ;
   //msgError="Invalid Credentials, Please Enter a Valid User Name And/or Password";
   msgError ="";
-  constructor(private _http : HttpClient, private router : Router,  public auth: AngularFireAuthModule, public _service : AuthserviceService) {
+  constructor(private _http : HttpClient, private router : Router,  public auth: AngularFireAuthModule, public _srvc : AuthserviceService) {
 
    }
 
@@ -50,51 +52,68 @@ export class LoginComponent implements OnInit {
     //window.localStorage.clear();
     // this.response=this._http.post("localhost:3000/login",this.user, this.Credentials );
     this.signInForm = new FormGroup({
-     
+
       'password': new FormControl('', Validators.required),
-      
+
       'username': new FormControl('', Validators.required)
     });
   }
 
   Loginusr(e: any){
-    if(e){
-      e.preventDefault(); //prevents default form behavior
-    }
-    let yy = this._service.loginUser({$username:this.signInForm.value.username}, {$password:this.signInForm.value.password})
-    .then((result) => {
-      if(result == null) //null result value means success
-        console.log("Successfully signed in user in firebase!"); 
-      else if(result.isValid == false) //false result means an error
-        this.firebaseErrorMessage = result.message 
-    });
+
     let user = {username: this.signInForm.value.username,
                 password: this.signInForm.value.password};
 
     let Credentials = {withCredentials: true};
-    this.auth = new firebase.auth.GoogleAuthProvider();
+    //this.auth = new firebase.auth.GoogleAuthProvider();
     console.log(this.auth);
-    console.log(this.username);
-    console.log(this.password);
+    console.log("before DB conn"+this.signInForm.value.username);
+    console.log("before DB conn"+this.signInForm.value.password);
     console.log(user);
     console.log(Credentials);
-
+    delay(5000);
     //window.localStorage.setItem("username",this.username);
     let response = this._http.post<any>("http://localhost:3000/login", user, httpOptions ).subscribe (
       {
-      next: (v) =>  this.getToken(),//this.router.navigate(['/frontpage']),  //console.log("reponse rcieved"),
+      next: (v) =>  this.getToken(e),//this.router.navigate(['/frontpage']),  //console.log("reponse rcieved"),
       error: (e) => console.error(this.msgError="Invalid Credentials, Please Enter a Valid User Name And/or Password"),
-      complete: () => window.localStorage.setItem("username",this.username)//console.info('Complete')
+      complete: () => window.localStorage.setItem("username",this.signInForm.value.username)//console.info('Complete')
     }
     );
   };
 
-  getToken(){
+  getToken(e: any){
     console.log("We have acquired Token")
+    if(e){
+      e.preventDefault(); //prevents default form behavior
+    }
+    let email=this.signInForm.value.username;
+    let passw= this.signInForm.value.password;
+    console.log(email);
+    console.log(passw);
+    delay(5000);
+    this.auth = new firebase.auth.GoogleAuthProvider();
+
+    //this._srvc.loginUser({username:this.signInForm.value.username}, {password:this.signInForm.value.password})
+    delay(5000);
+    this._srvc.loginUser(email, passw)
+    .then((result) => {
+      console.log("result object..:"+result);
+      if(result == null) //null result value means success
+        console.log("Successfully signed in user in firebase!");
+      else if(result.isValid == false) //false result means an error
+      {
+        console.log("conection to firebase return not logged")
+        this.firebaseErrorMessage = result.message
+      }
+    });
+
+
+
     this.router.navigate(['/frontpage'])
     //let id_token = googleUser.getAuthResponse().id_token;
 
-    
+
   }
 
 
--- FrontEnd/src/app/services/authservice.service.ts
+++ FrontEnd/src/app/services/authservice.service.ts
@@ -8,17 +8,17 @@ import * as firebase from 'firebase/compat';
   providedIn: 'root'
 })
 export class AuthserviceService {
-  userLoggedIn!: boolean; 
+  userLoggedIn!: boolean;
 
   constructor(private router: Router, private afAuth: AngularFireAuth) {
 
-    this.userLoggedIn = false; 
+    this.userLoggedIn = false;
 
     this.afAuth.onAuthStateChanged((user) => {
       if(user){
-        this.userLoggedIn = true; 
+        this.userLoggedIn = true;
       } else {
-        this.userLoggedIn = false; 
+        this.userLoggedIn = false;
       }
     });
    }
@@ -26,26 +26,27 @@ export class AuthserviceService {
    signupUser(user:any): Promise<any>{
     return this.afAuth.createUserWithEmailAndPassword(user.email, user.password)
     .then((result) => {
-      let emailLower = user.email.toLowerCase(); 
+      let emailLower = user.email.toLowerCase();
       result.user?.sendEmailVerification(); //send user email verification
     });
    }
 
-   loginUser(email: any, password: any): Promise<any> {
+   loginUser(email: string, password: string): Promise<any> {
 
-    let fireAuth = this.afAuth.signInWithEmailAndPassword(email, password)
-    
-    .catch(function(error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    if (errorCode === 'auth/wrong-password') {
-      alert('Wrong password.');
-    } else {
+    //let fireAuth = this.afAuth.signInWithEmailAndPassword(email, password)
+    this.afAuth.signInWithEmailAndPassword(email, password)
+    .catch(function(error)
+    {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      if (errorCode === 'auth/wrong-password') {
+        alert('Wrong password.');
+      } else {
       alert(errorMessage);
-  }
-  console.log(error);
-});
+    }
+    console.log(error);
+    });
 
     return this.afAuth.signInWithEmailAndPassword(email, password)
     .then(() => {
@@ -55,7 +56,7 @@ export class AuthserviceService {
 }
 
 function onSignIn(firebaseUser: { getAuthResponse: () => { (): any; new(): any; id_token: any; }; }){
-  let id_token = firebaseUser.getAuthResponse().id_token; 
+  let id_token = firebaseUser.getAuthResponse().id_token;
 }
 function signInWithEmailAndPassword(auth: any, email: string, password: string) {
   throw new Error('Function not implemented.');
